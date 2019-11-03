25f5d96cd00d722ee78ec5da0208bd789b560ff4
gregwhitaker <gwhitake@gmail.com>
2019-11-02 18:00:34 -0700

    Doing work

--- client/src/main/java/example/client/controller/ClientController.java
+++ client/src/main/java/example/client/controller/ClientController.java
@@ -1,12 +1,18 @@
 package example.client.controller;
 
 import example.client.model.ClientResponse;
+import example.model.LetterRequest;
+import example.model.LetterResponse;
+import example.model.NumberRequest;
+import example.model.NumberResponse;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ClientController {
@@ -21,29 +27,45 @@ public class ClientController {
      *
      * @return
      */
+    @GetMapping(value = "/letters",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Publisher<ClientResponse> getLetters(@RequestParam(name = "count", defaultValue = "10") int count) {
+        Flux<LetterResponse> randomLetters = letterRSocketRequester.route("randomLetters")
+                .data(new LetterRequest(count))
+                .retrieveFlux(LetterResponse.class);
+
+        return randomLetters.map(letterResponse -> new ClientResponse(Character.toString(letterResponse.getLetter())));
+    }
+
+    /**
+     *
+     * @return
+     */
     @GetMapping(value = "/numbers",
                 produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Publisher<ClientResponse> getNumbers() {
-        return null;
+    public Publisher<ClientResponse> getNumbers(@RequestParam(name = "count", defaultValue = "10") int count) {
+        Flux<NumberResponse> randomNumbers = numberRSocketRequester.route("randomNumbers")
+                .data(new NumberRequest(count))
+                .retrieveFlux(NumberResponse.class);
+
+        return randomNumbers.map(numberResponse -> new ClientResponse(Integer.toString(numberResponse.getNumber())));
     }
 
     /**
      *
      * @return
      */
-    @GetMapping(value = "/letters",
+    @GetMapping(value = "/both",
                 produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Publisher<ClientResponse> getLetters() {
-        return null;
-    }
+    public Publisher<ClientResponse> getNumbersAndLetters(@RequestParam(name = "count", defaultValue = "10") int count) {
+        Flux<LetterResponse> randomLetters = letterRSocketRequester.route("randomLetters")
+                .data(new LetterRequest(count))
+                .retrieveFlux(LetterResponse.class);
 
-    /**
-     *
-     * @return
-     */
-    @GetMapping(value = "/numbersletters",
-                produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Publisher<ClientResponse> getNumbersAndLetters() {
+        Flux<NumberResponse> randomNumbers = numberRSocketRequester.route("randomNumbers")
+                .data(new NumberRequest(count))
+                .retrieveFlux(NumberResponse.class);
+
         return null;
     }
 }
